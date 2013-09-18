b7741df1f24724fa11d6e5afce4eac1d5f90d651
Michael Dawson <m.dawson@y7mail.com>
2013-09-18 19:42:29 +0930

    Added csv split

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,9 +2,11 @@ class ResultsController < ApplicationController
   def index
   	@results = Result.all
 
-  	respond_to do |format|
-	    format.html
-	    format.csv { send_data @results.to_csv }
+  	if params[:offset] && params[:size]
+	  	respond_to do |format|
+		    format.html
+		    format.csv { send_data @results.to_csv(params[:offset].to_i, params[:size].to_i) }
+		end
 	end
   end
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,9 +1,9 @@
 class Result < ActiveRecord::Base
-
-	def self.to_csv(options = {})
-		CSV.generate(options) do |csv|
+	def self.to_csv(offset=0, size=1000)
+		puts "about to generate based on #{size} size and #{offset} offset"
+		CSV.generate() do |csv|
 			csv << column_names
-			all.each do |result|
+			offset(offset).first(size).each do |result|
 				csv << result.attributes.values_at(*column_names)
 			end
 		end
